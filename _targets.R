82bf62ebb03d2be85314388e78e6cb3261bc893f
sondreskarsten <sskarste@uchicago.edu>
2025-03-12 02:14:17 +0000

    test

---
+++ _targets.R
@@ -0,0 +1,49 @@
+
+library(targets)
+library(httr)
+library(googledrive)
+library(arrow)
+library(future)
+
+plan(multisession, workers = 2)
+
+tar_option_set(packages = c("httr", "googledrive", "arrow"), deployment = "worker")
+
+download_file <- function(url) {
+  file <- tempfile(fileext = ".csv")
+  GET(url, write_disk(file, overwrite = TRUE))
+  file
+}
+
+csv_to_parquet <- function(csv_file, type, base_path = "data") {
+  date <- format(Sys.Date(), "%Y-%m-%d")
+  parquet_path <- file.path(base_path, paste0(type, "_", date, ".parquet"))
+  dir.create(dirname(parquet_path), recursive = TRUE, showWarnings = FALSE)
+  
+  df <- read.csv(csv_file)
+  write_parquet(df, parquet_path)
+  parquet_path
+}
+
+upload_to_drive <- function(file, folder_name = "Brreg_Data") {
+  drive_auth()
+
+  folder <- drive_get(folder_name)
+  if (nrow(folder) == 0) {
+    folder <- drive_mkdir(folder_name)
+  }
+
+  drive_upload(file, path = folder, name = basename(file), overwrite = TRUE)
+}
+
+list(
+  tar_target(enhets_csv, download_file("https://data.brreg.no/enhetsregisteret/api/enheter/lastned/csv"), format = "file"),
+  tar_target(underenhets_csv, download_file("https://data.brreg.no/enhetsregisteret/api/underenheter/lastned/csv"), format = "file"),
+
+  tar_target(enhets_parquet, csv_to_parquet(enhets_csv, "EnhetsRegisteret"), format = "file"),
+  tar_target(underenhets_parquet, csv_to_parquet(underenhets_csv, "UnderEnhetsRegisteret"), format = "file"),
+
+  tar_target(upload_enhets_parquet, upload_to_drive(enhets_parquet)),
+  tar_target(upload_underenhets_parquet, upload_to_drive(underenhets_parquet))
+)
+
---
+++ targets_pipeline.R
@@ -0,0 +1,42 @@
+
+library(targets)
+library(httr)
+library(googledrive)
+
+tar_option_set(packages = c("httr", "googledrive"))
+
+download_brreg_data <- function() {
+  enhets_url <- "https://data.brreg.no/enhetsregisteret/api/enheter/lastned/csv"
+  underenhets_url <- "https://data.brreg.no/enhetsregisteret/api/underenheter/lastned/csv"
+  
+  enhets_file <- tempfile(fileext = ".csv")
+  underenhets_file <- tempfile(fileext = ".csv")
+  
+  GET(enhets_url, write_disk(enhets_file, overwrite = TRUE))
+  GET(underenhets_url, write_disk(underenhets_file, overwrite = TRUE))
+  
+  list(enhets_file = enhets_file, underenhets_file = underenhets_file)
+}
+
+upload_to_drive <- function(files, folder_name = "Brreg_Data") {
+  drive_auth()
+  
+  folder <- drive_get(folder_name)
+  if (nrow(folder) == 0) {
+    folder <- drive_mkdir(folder_name)
+  }
+  
+  date_suffix <- format(Sys.Date(), "%Y-%m-%d")
+  
+  enhets_drive_name <- paste0("EnhetsRegisteret_", date_suffix, ".csv")
+  underenhets_drive_name <- paste0("UnderEnhetsRegisteret_", date_suffix, ".csv")
+  
+  drive_upload(media = files$enhets_file, path = folder, name = enhets_drive_name, overwrite = TRUE)
+  drive_upload(media = files$underenhets_file, path = folder, name = underenhets_drive_name, overwrite = TRUE)
+}
+
+list(
+  tar_target(brreg_files, download_brreg_data(), format = "file"),
+  tar_target(upload_result, upload_to_drive(brreg_files))
+)
+
